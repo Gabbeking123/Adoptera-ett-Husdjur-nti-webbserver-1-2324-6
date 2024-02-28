19b84c6f4cda106e527014d0000fcb7c21ed12b8
Ali Adil Najm <128463366+Ali-109@users.noreply.github.com>
2024-02-28 09:18:39 +0000

    update

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
+"""Adopt a pet application using Flask.
+
+This Flask application allows users to browse and view information about different types of pets, including dogs, cats, and rabbits.
+The application uses a separate helper.py file to store the pet data.
+
+The application has the following routes:
+
+- `/`: Displays the home page with links to the different types of pets.
+- `/animals/<string:pet_type>`: Displays a list of all pets of the specified type.
+- `/animals/<string:pet_type>/<int:pet_id>`: Displays the details of the specified pet.
+
+The application is started by running the `app.run()` function.
+"""
+
 from flask import Flask
+
+from helper import pets
 
 app = Flask(__name__)
 
 
+# Route to display the home page
 @app.route('/')
 def index():
   return '''
@@ -10,19 +27,49 @@ def index():
   <p>Browse through the links below to find your new furry friend:<p/> 
 <ul>
 <li><a href='/animals/dogs'>Dogs</a></li> 
-<li><a href='/animals/cats'>cats </a> </li> 
-<li><a href='/animals/rabbits'>rabbits</a></li> 
+<li><a href='/animals/cats'>Cats </a> </li> 
+<li><a href='/animals/rabbits'>Rabbits</a></li> 
 </ul>
-  
+
   '''
 
 
+# Route to display the list of pets based on pet type
 @app.route('/animals/<string:pet_type>')
+# This function displays a list of all pets of the specified type.
 def animals(pet_type):
   html = f'<h1>List of {pet_type}<h1/>'
+  html += '<ul>'
+  for id, pet in enumerate(pets[pet_type]):
+    html += f'<li><a href="/animals/{pet_type}/{id}">{pet["name"]}</a></li>'
   return html
 
 
-# Viktigt: Denna kodrad ska alltid placeras längst ner i filen.
-# Detta för att säkerställa en korrekt uppstart av servern.
+@app.route('/animals/<string:pet_type>/<int:pet_id>')
+# This function displays details about a specific pet.
+def pet(pet_id, pet_type):
+  """Display details about a specific pet.
+  Args: 
+      pet_id (int): The ID of the pet to display.
+      pet_type (string): The type of pet to display.
+  Returns: 
+      A formatted string with the details of the specified pet.
+  """
+  list_pet_type = pets[pet_type]
+  pet = list_pet_type[pet_id]
+  return f'''
+  <h1> {pet["name"]}</h1>
+
+  <img src={pet["url"]} /> 
+
+  <p>{pet["description"]}</p>
+
+  <ul>
+    <li>{pet["breed"]} </li>
+    <li>{pet["age"]} </li>
+  </ul>
+  '''
+
+
+# This line is important for starting the Flask server
 app.run(debug=True, host="0.0.0.0")
---
+++ kladd.md
@@ -0,0 +1,21 @@
+pets = {dogs : [{HUND 1} {HUND 2}] , rabbit: ... , cats: ...}  
+
+pets.dogs
+pets['dogs']
+
+pets['cat']
+
+
+lista = pets['dogs']
+
+lista[0]  ==> {Hund 1}
+
+
+hund1 = lista[0]
+
+
+_____
+hund1['name'] ==> name för hund1
+
+
+
